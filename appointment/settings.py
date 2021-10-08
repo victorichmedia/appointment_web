d81c3f4eb2cfc3e9ef2f7804a1b06d20f3482344
victorichmedia <aladevictortimothy@gmail.com>
2021-10-08 08:03:06 -0700

    Added Appointment model

--- appointment/settings.py
+++ appointment/settings.py
@@ -111,6 +111,8 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
+TIME_INPUT_FORMATS = '%H:%M'
+
 USE_I18N = True
 
 USE_L10N = True
--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from typing import ByteString
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
@@ -47,3 +48,33 @@ class User(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return self.is_staff
+
+
+class Appointment(models.Model):
+
+    BEST_METHOD_FOR_CONTACT = [
+        ("Phone", "Phone"),
+        ("Email", "Email"),
+    ]
+
+    BEST_TIME_OF_THE_DAY_TO_REACH_YOU = [
+        ("Morning", "Morning"),
+        ("Noon", "Noon"),
+        ("Afternoon", "Afternoon"),
+        ("Evening", "Evening"),
+        ("Night", "Night"),
+    ]
+
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    date = models.DateField()
+    time = models.TimeField()
+    contact_method = models.CharField(" Best method for contact", max_length=6, choices=BEST_METHOD_FOR_CONTACT)
+    time_of_the_day_to_reach = models.CharField("Best time of the day to reach you", max_length=20, choices=BEST_TIME_OF_THE_DAY_TO_REACH_YOU )
+    how_can_we_help_you = models.TextField()
+    additional_notes = models.TextField()
+
+    def __str__(self):
+        return f"{self.lastname} {self.firstname}"
